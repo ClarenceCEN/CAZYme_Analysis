2ef0271ddf36d712ddcb42bfbd4ff887e368f40c
ClarenceCen <387952081@qq.com>
2019-07-12 15:42:03 -0500

    keep on modifying header

---
+++ build_database/modifiy_header.py
@@ -0,0 +1,23 @@
+import re
+import os
+
+for f in os.listdir(os.getcwd()):
+    if f.endswith('faa'):
+        print(f)
+        with open(f, 'r') as f1:
+            f_modified_path = f.replace('.faa','_modified.faa')
+            if os.path.exists(f_modified_path):
+                os.remove(f_modified_path)
+            with open(f_modified_path, 'a') as f2:
+                i = 1
+                line = f1.readline()
+                while (line):
+                    if line[0] == '>':
+                        print('That is the header!')
+                        f2.write(">gene_%d\n" % i)
+                        print(">gene_%d\n" % i)
+                        i += 1
+                    else:
+                        f2.write(line)
+                        #print(line)
+                    line = f1.readline()
