9b5c1d943b81ecf5b574445023cb829d81a5a6be
ClarenceCen <387952081@qq.com>
2019-07-13 16:53:25 -0500

    correct subfamily

---
+++ build_database/cazy_annotation_temp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+for file in ./*_modified.faa
+do
+name=`basename $file`
+output_name="${name%.*}.out"
+if [ ! -f "$output_name" ]; then
+	echo $output_name
+	echo '----'${name%.*}'-----'
+	/plus/work/soft/hmmer-3.1/binaries/hmmscan --cpu 1 --domtblout ${name%.*}.out /plus/work/soft/CAZY/dbCAN-fam-HMMs.txt ${name%.*}.faa >${name%.*}.out2
+	rm ${name%.*}.out2
+fi
+bash hmmscan-parser.sh ${name%.*}.out > ${name%.*}.tab
+done
+
