bebb38838240abd593bb96068dc6a1a0f85437ee
ClarenceCen <387952081@qq.com>
2019-07-13 17:51:29 -0500

    changed the structure of the code

--- build_database/build_database.py
+++ build_database/build_database.py
@@ -11,6 +11,7 @@ def make_agr_parser():
     parser.add_argument('-l','--level',help='The level tab file.')
     return parser
 
+'''
 def find_value_recursive(cazy_dict,cazyme):
     global flag
     global levels
@@ -34,6 +35,7 @@ def find_value_recursive(cazy_dict,cazyme):
             global n_level
             n_level = n+1
             return
+'''
 
 def build_database(table,seq,output,level):
     tab_file = pd.read_csv(table, sep='\t', header=None)
@@ -43,12 +45,12 @@ def build_database(table,seq,output,level):
     with open(seq, 'r') as f:
         seq_file = f.read()
 
-    CAZYme_family = {'CAZyme':{'Enzyme_Classes':{'GlycosylTransferases':'GT',
-              'GlycosideHydrolases':'GH',
-              'CarbohydrateEsterases':'CE',
-              'PolysaccharideLyases':'PL',
-              'AuxiliaryActivities':'AA'},
-              'Associated_Modules':{'Carbohydrate-BindingModules':'CBM'}}}
+    #CAZYme_family = {'CAZyme':{'Enzyme_Classes':{'GlycosylTransferases':'GT',
+    #          'GlycosideHydrolases':'GH',
+    #          'CarbohydrateEsterases':'CE',
+    #          'PolysaccharideLyases':'PL',
+    #          'AuxiliaryActivities':'AA'},
+    #          'Associated_Modules':{'Carbohydrate-BindingModules':'CBM'}}}
 
     basename = os.path.splitext(os.path.basename(seq))[0]
 
@@ -61,75 +63,71 @@ def build_database(table,seq,output,level):
     if os.path.exists(output_txt):
         os.remove(output_txt)
 
-    for i in range(0, tab_file.shape[0]):
-        global n, n_level
-        n = 0
-        n_level = 0
+    with open(output_fasta,'a') as f:
+        for i in range(0, tab_file.shape[0]):
+            #global n, n_level
+            #n = 0
+            #n_level = 0
+            family = tab_file.loc[i, 'Family_HMM'].replace('.hmm', '')
+            query_id = tab_file.loc[i, 'Query_ID']
+            query_start = tab_file.loc[i, 'Query_start']
+            query_end = tab_file.loc[i, 'Query_end']
+            query_length = tab_file.loc[i, 'Query_length']
+            #print('%d: %s' % (i + 1, family))
+            seq_pattern = re.compile(r'>' + query_id + '\n(.*?)(>|\n$)', re.S)  #we cannot replace . with [A-Z]
+            try:
+                query_sequence = re.search(seq_pattern, seq_file).group(1).replace('\n', '')
+            except:
+                print('Did not find %s in sequence!' % family)
+                continue
 
-        family = tab_file.loc[i, 'Family_HMM'].replace('.hmm', '')
-        query_id = tab_file.loc[i, 'Query_ID']
-        query_start = tab_file.loc[i, 'Query_start']
-        query_end = tab_file.loc[i, 'Query_end']
-        query_length = tab_file.loc[i, 'Query_length']
-        print('%d: %s' % (i + 1, family))
-        seq_pattern = re.compile(r'>' + query_id + '.*?\n(.*?)(>|\n$)', re.S)
-        try:
-            query_sequence = re.search(seq_pattern, seq_file).group(1).replace('\n', '')  # does \n count as char?
-        except:
-            print('Did not find %s in sequence!' % family)
-            continue
+            if (len(query_sequence) != query_length):
+                print("The length of %s did not match! Please check the input seqs!" % family)
+                continue
+            #print('Query Sequence: %s' % query_sequence)
+            target_sequence = query_sequence[query_start - 1:query_end]
+            #print('Target Seuqunce: %s\n' % target_sequence)
 
-        if (len(query_sequence) != query_length):
-            print("The length of %s did not match! Please check the input seqs!" % family)
-            continue
-        print('Query Sequence: %s' % query_sequence)
-        target_sequence = query_sequence[query_start - 1:query_end]
-        print('Target Seuqunce: %s\n' % target_sequence)
-
-        with open(output_fasta, 'a') as f:
             f.write('>cazy_%04d_%s\n%s\n' % (i, query_id, target_sequence))
 
 
-
-        name_pattern = re.compile(r'(GH|GT|CBM|PL|AA|CE)(\d+)(_\d+)?')
-        try:
-            cazy_cat = re.search(name_pattern, family).group(1)
-            cazy_num = re.search(name_pattern, family).group(2)
-        except:
-            print('Find %s!' % family)
-            print("Interesting family! What is that?")
-            continue
-
-        '''
-        # method 1    
-        cazy_prefix = find_value_recursive(CAZYme_family,cazy_cat)
-        cazy_tax = cazy_prefix+cazy_num
-        if cazy_cat == 'GH' and int(cazy_num) in [5,13,30,43]:
+    with open(output_txt,'a') as f:
+        for i in range(0, tab_file.shape[0]):
+            name_pattern = re.compile(r'(GH|GT|CBM|PL|AA|CE)(\d+)(_\d+)?')
+            family = tab_file.loc[i, 'Family_HMM'].replace('.hmm', '')
             try:
-                cazy_sub_num = re.search(name_pattern,family).group(3)
+                cazy_cat = re.search(name_pattern, family).group(1)
+                cazy_num = re.search(name_pattern, family).group(2)
             except:
-                print('%s%s had no sub family.'%(cazy_cat,cazy_num))
-            cazy_tax = cazy_tax+';L'+str(n_level+1)+'_'+family
-        '''
+                print('Find %s! What is that?' % family)
+                continue   #Shall we continue?
 
-        # method 2
-        cazy_tab = pd.read_csv(level,seq='\t')
-        temp = cazy_tab.loc[cazy_tab.loc[:, 'L4'] == cazy_cat, :]
-        cazy_tax = ''
-        for i in range(0, cazy_tab.shape[1]):
-            cazy_tax = cazy_tax + temp.columns[i] + '_' + temp.iloc[0, i] + ';'
-        cazy_tax = cazy_tax.strip(';') + cazy_num
-        if cazy_cat == 'GH' and int(cazy_num) in [5, 13, 30, 43]:
-            try:
-                cazy_sub_num = re.search(name_pattern, family).group(3)
-            except:
-                print('%s%s had no sub family.' % (cazy_cat, cazy_num))
-            cazy_tax = cazy_tax + ';L5_' + family
+            '''
+            # method 1    
+            cazy_prefix = find_value_recursive(CAZYme_family,cazy_cat)
+            cazy_tax = cazy_prefix+cazy_num
+            if cazy_cat == 'GH' and int(cazy_num) in [5,13,30,43]:
+                try:
+                    cazy_sub_num = re.search(name_pattern,family).group(3)
+                except:
+                    print('%s%s had no sub family.'%(cazy_cat,cazy_num))
+                cazy_tax = cazy_tax+';L'+str(n_level+1)+'_'+family
+            '''
 
-        print(cazy_tax)
-        print('\n')
+            # method 2
+            cazy_tab = pd.read_csv(level,sep='\t')
+            temp = cazy_tab.loc[cazy_tab.loc[:, 'L4'] == cazy_cat, :]
+            cazy_tax = ''
+            for i in range(0, cazy_tab.shape[1]):
+                cazy_tax = cazy_tax + temp.columns[i] + '_' + temp.iloc[0, i] + ';'
+            cazy_tax = cazy_tax.strip(';') + cazy_num
+            if cazy_cat == 'GH' and int(cazy_num) in [5, 13, 30, 43]:
+                if re.search(name_pattern, family).group(3):
+                    cazy_tax = cazy_tax + ';L5_' + family
 
-        with open(output_txt, 'a') as f:
+            #print(cazy_tax)
+            #print('\n')
+
             f.write('cazy_%04d_%s\t%s\n' % (i, query_id, cazy_tax))
 
 def main():
@@ -146,7 +144,8 @@ def main():
         os.makedirs(output_path)
 
     for f in os.listdir(input_path):
-        if os.path.splitext(f)[1]=='tab':
+        #print(os.path.splitext(f))
+        if os.path.splitext(f)[1]=='.tab':
             print(os.path.splitext(f)[0])
             pro_seq = os.path.splitext(f)[0]+'.faa'
             dbCan_table = f
