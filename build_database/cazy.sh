4b10bc903227deb5d22c4868e7f860512ef69bd6
ClarenceCen <387952081@qq.com>
2019-07-16 16:43:19 -0500

    a big try!
    try to use basename to delete the suffix

--- build_database/cazy.sh
+++ build_database/cazy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#go to the assembled fasta directory
+
+cd /project/flatiron2/cen/cazy_database
 
 for f in *.fasta;
 do
@@ -8,10 +11,10 @@ done
 
 #The protein prediction part.
 #I used GeneMark.
-for file in ./fasta
+for file in ./.fasta
 do
-name=`basename $file`
-perl /plus/work/soft/genemark_suite_linux_64/gmsuite/gmsn.pl --prok --format GFF --fnn --faa ${name%.*}.fasta
+name=`basename $file .fasta`
+perl /plus/work/soft/genemark_suite_linux_64/gmsuite/gmsn.pl --prok --format GFF --fnn --faa $name.fasta
 done
 
 
@@ -22,11 +25,11 @@ python modify_header.py   #change the header of faa and fnn files.
 
 for file in ./*_modified.faa
 do
-name=`basename $file`
-output_name="${name%.*}.out"
+name=`basename $file .faa`
+output_name="$name.out"
 if [ ! -f "$output_name" ]; then
 	echo $output_name
-	echo '----'${name%.*}'-----'
+	echo '----'$name'-----'
 	/plus/work/soft/hmmer-3.1/binaries/hmmscan --cpu 1 --domtblout ${name%.*}.out /plus/work/soft/CAZY/dbCAN-fam-HMMs.txt ${name%.*}.faa >${name%.*}.out2
 	rm ${name%.*}.out2
 fi
@@ -34,3 +37,18 @@ bash hmmscan-parser.sh ${name%.*}.out > ${name%.*}.tab
 done
 
 python build_database.py -i ./ -o output/ -l cazy_level_tab.txt
+
+cd output/
+
+for f in *.*;
+do
+mv $f ${f/_modified/}
+done
+
+cd /project/flatiron2/cen/burst_output
+
+for f in /project/flatiron2/cen/cazy_database/output/*.txt;
+do
+
+mkdir 
+
