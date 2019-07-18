e32e4e307e0c97700c38394acc34181512b63e36
ClarenceCen <clarence_cen@yeah.net>
2019-07-18 11:15:02 -0500

    Update count_cazymes.py
    Added a taxonomy column

--- data_analysis/count_cazymes.py
+++ data_analysis/count_cazymes.py
@@ -22,11 +22,11 @@ def main():
     input_path = os.path.abspath(args.input)
     output_path = os.path.abspath(args.output)
 
-    temp_mean = pd.DataFrame()
-    temp_total = pd.DataFrame()
+    #temp_mean = pd.DataFrame()
+    #temp_total = pd.DataFrame()
 
     for dir in os.listdir(input_path):
-        #print(dirs)
+        #print(dir)
         temp = pd.DataFrame()
         for f in os.listdir(os.path.join(input_path,dir)):
             try:
@@ -35,26 +35,28 @@ def main():
                     print(f)
                     f_table = pd.read_csv(os.path.join(input_path,dir,f), sep='\t', header=None)
                     f_table.loc[:, 13] = f[:-3]
-                    f_table = f_table.loc[:, [12, 13]]
+                    f_table = f_table.loc[:, [12, 13,1]]
                     temp = pd.concat([temp,f_table],axis=0,ignore_index=True)
             except:
                 continue
-        temp.columns = ['#CAZyme', 'SampleID']
+        temp.columns = ['taxonomy', 'SampleID','#queryid']
         temp['count'] = 1
-        print(temp)
 
-        temp_wide = temp.pivot_table(index='SampleID', columns='#CAZyme', values='count', aggfunc='sum')
+
+        temp_wide = temp.pivot_table(index='SampleID', columns=['taxonomy','#queryid'], values='count', aggfunc='sum')
         temp_wide[temp_wide.isnull()] = 0.0
         temp_wide.T.to_csv(os.path.join(output_path,dir)+'.txt',sep='\t')
 
-        temp_wide_mean = temp_wide.mean()
-        temp_wide_mean.columns = ['CAZyme',dir]
+        #temp_wide_mean = temp_wide.mean()
+        #temp_wide_mean.columns = ['CAZyme',dir]
 
-        temp_mean = pd.merge([temp_mean, temp_wide_mean], on='#CAZyme')
-        temp_total = pd.merge([temp_total, temp.T], on='#CAZyme')
+        #print(temp_wide_mean)
 
-    #temp_mean.to_csv(os.path.join(output_path,'CAzyme_mean.txt'))
+        #temp_mean = pd.concat([temp_mean, temp_wide_mean],axis=1)
+        #temp_total = pd.concat([temp_total, temp.T],axis=1)
 
+    #temp_mean.to_csv(os.path.join(output_path,'CAzyme_mean.txt'),sep='\t')
+    #temp_mean.to_csv(os.path.join(output_path, 'CAzyme_total.txt'),sep='\t')
 
 
 
